e97f0dbaecbf04af64b6f0a810f84585abcb5880
SwS651 <sweiliang1006@gmail.com>
2024-01-04 00:19:27 +0800

    Updated login method

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # flask project
- 
+Create environment
+python -m venv venv
+
+run virtual environment in command prompt
+venv\Scripts\activate.bat
+
+<!-- Run Flask -->
+Python -m flask run
+
+<!-- Used to automatically run/update after saving -->
+python -m flask --debug run
--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db.init_app(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable = False)
     email = db.Column(db.String)
 
 with app.app_context():
@@ -19,29 +20,41 @@ with app.app_context():
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Worlds!ssa</p>"
+    return "<p>Hello, Worlds!sssa</p>"
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def user_list():
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("login.html")
+    info_message = None
+    url_direction = "users/login.html"
+    if request.method == "POST":
+        # users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+        user = db.session.execute(db.select(User).filter_by(username=request.form["username"],password=request.form["password"])).scalar()
+        if not user:
+            info_message = "Username or Password incorrect"
+        else:
+            url_direction = "index.html"
+    return render_template(f"{url_direction}", info_message = info_message)
 
 
-@app.route("/users/create", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def user_create():
     error = None
     if request.method == "POST":
         user = User(
             username=request.form["username"],
-            email=request.form["email"],
+            password = request.form["password"],
+            email=request.form["email"]
         )
         db.session.add(user)
         db.session.commit()
         # return redirect(url_for("user_detail", id=user.id))
-        return render_template("register.html",error = "register successfully" )
+        return render_template("users/register.html",error = "register successfully" )
 
-    return render_template("register.html",error = error )
+    return render_template("users/register.html",error = error )
+
+
+
 
 @app.route("/user/<int:id>")
 def user_detail(id):
